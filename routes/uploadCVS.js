798675e687de5cfb53d27ad058b873d21356b8bc
Trace-weik <tweik730@gmail.com>
2024-11-22 09:05:03 -0500

    New changes

--- routes/uploadCVS.js
+++ routes/uploadCVS.js
@@ -424,6 +424,7 @@ try {
 
         if(ownedCollectable.length > 0)
             await trx.insert(collectables).values(ownedCollectable);
+        
         console.log("Finished Creating attributes");
         console.log("All data inserted succesfully");
         res.status(200).send({ message: 'Data inserted successfully' });
